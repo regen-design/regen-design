e9a59bf4c02d2c4faae4fcb1a75a8b8ce17ab92d
“muddyrain” <qiu3291002845@gmail.com>
2024-01-16 19:57:32 +0800

    style(custom): themes package add color variables

---
+++ packages/theme/variable/index.tsx
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  colors: {
+    primary: '#ea580c',
+    danger: '#dc2626',
+    success: '#16a34a',
+    warning: '#fbbf24',
+    info: '#3b82f6',
+    dark: '#1f2937',
+    light: '#f3f4f6',
+  },
+}
