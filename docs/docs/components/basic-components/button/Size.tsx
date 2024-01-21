932c0d37010fc4af8e63881f720d64e757cdb837
muddyrain <qiu3291002845@gmail.com>
2024-01-21 17:22:16 +0800

    style(docs): modify docs document icon logo

--- docs/docs/components/basic-components/button/Size.tsx
+++ docs/docs/components/basic-components/button/Size.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@regen-design/components'
+import { Button, Space } from '@regen-design/components'
 
 export default function Demo() {
   return (
-    <div style={{ display: 'flex', gap: 10 }}>
+    <Space>
       <Button size="tiny">tiny</Button>
       <Button size="small">small</Button>
       <Button size="default">default</Button>
       <Button size="large">large</Button>
-    </div>
+    </Space>
   )
 }
