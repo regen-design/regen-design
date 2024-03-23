c75b620d1b85c61fa6e4cf24bbeca8b92363dce4
muddyrain <qiu3291002845@gmail.com>
2024-03-23 21:52:04 +0800

    fix(tabler): fix tabler custom attribute error reporting issue

--- packages/components/table/index.tsx
+++ packages/components/table/index.tsx
@@ -4,6 +4,8 @@ import { StyledTable, StyledTablePrefixClass as prefixClass } from '@regen-desig
 import classNames from 'classnames'
 import Markdown from 'react-markdown'
 import { Empty } from '../empty'
+import isPropValid from '@emotion/is-prop-valid'
+import { StyleSheetManager } from 'styled-components'
 export const Table: FC<TableProps> = ({
   style = {},
   className = '',
@@ -21,65 +23,67 @@ export const Table: FC<TableProps> = ({
   const trClass = classNames(`${prefixClass}-tr`)
 
   return (
-    <StyledTable role="table" className={tableClass} style={style} bordered={bordered}>
-      <div className={`${prefixClass}-wrapper`}>
-        <div className={`${prefixClass}-inner`}>
-          {columns.length > 0 && (
-            <thead className={theadClass}>
-              <tr className={trClass}>
-                {columns.map((column, index) => {
-                  const thClass = classNames(`${prefixClass}-th`)
-                  const { title, dataIndex, key, width, align } = column
+    <StyleSheetManager shouldForwardProp={isPropValid}>
+      <StyledTable role="table" className={tableClass} style={style} bordered={bordered}>
+        <div className={`${prefixClass}-wrapper`}>
+          <div className={`${prefixClass}-inner`}>
+            {columns.length > 0 && (
+              <thead className={theadClass}>
+                <tr className={trClass}>
+                  {columns.map((column, index) => {
+                    const thClass = classNames(`${prefixClass}-th`)
+                    const { title, dataIndex, key, width, align } = column
+                    return (
+                      <th
+                        className={thClass}
+                        key={key || dataIndex || index}
+                        style={{ width, textAlign: align }}
+                      >
+                        {title}
+                      </th>
+                    )
+                  })}
+                </tr>
+              </thead>
+            )}
+            {dataSource.length > 0 && (
+              <tbody className={tbodyClass}>
+                {dataSource.map((row, index) => {
+                  const trKey = row[rowKey] || index
                   return (
-                    <th
-                      className={thClass}
-                      key={key || dataIndex || index}
-                      style={{ width, textAlign: align }}
-                    >
-                      {title}
-                    </th>
+                    <tr key={trKey} className={trClass}>
+                      {columns.map((column, columnIndex) => {
+                        const tdClass = classNames(`${prefixClass}-td`)
+                        const { dataIndex, key, width, align, render } = column
+                        const value = row[dataIndex]
+                        const content = render ? render(value, row, index) : value
+                        return (
+                          <td
+                            className={tdClass}
+                            key={key || dataIndex || columnIndex}
+                            style={{ width, textAlign: align }}
+                          >
+                            {markdown ? (
+                              <Markdown className={`${prefixClass}-markdown`}>{content}</Markdown>
+                            ) : (
+                              content
+                            )}
+                          </td>
+                        )
+                      })}
+                    </tr>
                   )
                 })}
-              </tr>
-            </thead>
-          )}
-          {dataSource.length > 0 && (
-            <tbody className={tbodyClass}>
-              {dataSource.map((row, index) => {
-                const trKey = row[rowKey] || index
-                return (
-                  <tr key={trKey} className={trClass}>
-                    {columns.map((column, columnIndex) => {
-                      const tdClass = classNames(`${prefixClass}-td`)
-                      const { dataIndex, key, width, align, render } = column
-                      const value = row[dataIndex]
-                      const content = render ? render(value, row, index) : value
-                      return (
-                        <td
-                          className={tdClass}
-                          key={key || dataIndex || columnIndex}
-                          style={{ width, textAlign: align }}
-                        >
-                          {markdown ? (
-                            <Markdown className={`${prefixClass}-markdown`}>{content}</Markdown>
-                          ) : (
-                            content
-                          )}
-                        </td>
-                      )
-                    })}
-                  </tr>
-                )
-              })}
-            </tbody>
+              </tbody>
+            )}
+          </div>
+          {dataSource.length === 0 && (
+            <div className={`${prefixClass}-empty`}>
+              <Empty />
+            </div>
           )}
         </div>
-        {dataSource.length === 0 && (
-          <div className={`${prefixClass}-empty`}>
-            <Empty />
-          </div>
-        )}
-      </div>
-    </StyledTable>
+      </StyledTable>
+    </StyleSheetManager>
   )
 }
